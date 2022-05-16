37d5770da61faf5f0b654f5d320f578177ea37df
Bruno Lages <brunok1lages@gmail.com>
2022-05-15 22:58:04 -0300

    Responsividade Tela de cadastro
    Pronta, mas da pra melhorar

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -23,22 +23,23 @@ function Cadastro(){
                     <h1>Cadastro</h1>
                 </div>
                 <div className="texto">Para criar sua conta, por favor preencha os dados abaixo: </div>
-          
-                <input type="text" placeholder ="Nome" name ="nome"
+                <div className='dados'>
+                    <input type="text" placeholder ="Nome" name ="nome"
+                            onChange={UpdateUser}/>
+                    {/* Setando a característica Nome do objeto*/}
+                    <input type="text" placeholder ="CPF" name = "cpf" 
                         onChange={UpdateUser}/>
-                {/* Setando a característica Nome do objeto*/}
-                <input type="text" placeholder ="CPF" name = "cpf" 
-                    onChange={UpdateUser}/>
-                <input type="text" placeholder ="Senha" name = "senha" 
-                    onChange={UpdateUser}/>
-                <input type="text" placeholder ="Confirme sua senha" name = "confsenha" 
-                    onChange={UpdateUser}/>
-                <input type="text" placeholder ="Email" name = "email" 
-                    onChange={UpdateUser}/>
-                <input type="text" placeholder ="Telefone" name = "telefone" 
-                    onChange={UpdateUser}/>
-                <input type="text" placeholder ="Endereço" name = "endereco" 
-                    onChange={UpdateUser}/>
+                    <input type="text" placeholder ="Senha" name = "senha" 
+                        onChange={UpdateUser}/>
+                    <input type="text" placeholder ="Confirme sua senha" name = "confsenha" 
+                        onChange={UpdateUser}/>
+                    <input type="text" placeholder ="Email" name = "email" 
+                        onChange={UpdateUser}/>
+                    <input type="text" placeholder ="Telefone" name = "telefone" 
+                        onChange={UpdateUser}/>
+                    <input type="text" placeholder ="Endereço" name = "endereco" 
+                        onChange={UpdateUser}/>
+                </div>
                 <div className = "buttonContent"><button className="cadbutton"
                 onClick={()=>{alert("Cadastro realizado com sucesso!")}}>CADASTRAR</button></div>
                 
