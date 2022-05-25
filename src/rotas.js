77369261eb94601a5ac02e6aaa4bc6d5bcc7fc8f
Bruno Lages <brunok1lages@gmail.com>
2022-05-25 18:52:46 -0300

    + Pequenos ajustes

--- src/rotas.js
+++ src/rotas.js
@@ -7,36 +7,35 @@ import PreLoad from './pages/PreLoad';
 import MeuPerfil from './pages/MeuPerfil';
 import AnimaisParaAdocao from './pages/AnimaisParaAdocao';
 import HeaderFooter from './HeaderFooter/HeaderFooter';
-import {isAuthenticated} from "./services/auth";
+import { isAuthenticated } from "./services/auth";
 import Contato from './pages/Contato';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route
-      {...rest}
-      render={props =>
-        isAuthenticated() ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
-        )
-      }
-    />
-  );
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
+      )
+    }
+  />
+);
 
-function Rotas(){
-    return (
-        <BrowserRouter>
-            <HeaderFooter>
-                {/* <Route component={() => <Redirect to="/preload" />} /> */}
-                <Route path='/preload' component={PreLoad} />
-                <PrivateRoute path='/animais-para-adocao' component={AnimaisParaAdocao} />
-                <PrivateRoute path='/meu-perfil' component={MeuPerfil} />
-                <Route path='/home' component={Home} />
-                <Route path='/login' component={Login} />
-                <Route path='/cadastro' component={Cadastro} />
-                <Route path='/contato' component={Contato} />
-            </HeaderFooter>
-        </BrowserRouter>
-    );
+function Rotas() {
+  return (
+    <BrowserRouter>
+      <HeaderFooter>
+        <Route component={() => <Redirect to="/home" />} /> 
+        <Route path='/animais-para-adocao' component={AnimaisParaAdocao} />
+        <Route path='/meu-perfil' component={MeuPerfil} />
+        <Route path='/home' component={Home} />
+        <Route path='/login' component={Login} />
+        <Route path='/cadastro' component={Cadastro} />
+        <Route path='/contato' component={Contato} />
+      </HeaderFooter>
+    </BrowserRouter>
+  );
 }
 export default Rotas;
